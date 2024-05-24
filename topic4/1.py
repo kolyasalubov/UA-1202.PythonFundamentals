3e47fa3b86a494b69965f13441cbabc5c6a8fef3
Liubov Koliasa <kolyasa.lubov@gmail.com>
2024-05-24 17:30:20 +0300

    added tipic 4

---
+++ topic4/1.py
@@ -0,0 +1,45 @@
+# a = 50
+
+
+
+# if 0 < a < 40:
+#     print("Hello")
+#     print("Second")
+
+# print("The End")
+###########################################################
+# temperature = float(input('What is the temperature? '))
+
+# if temperature > 30:
+#     print('Wear shorts.')
+#     print("999")
+# else:
+#     print('Wear long pants.')
+
+# print('Get some exercise outside.')
+#############################################################
+# print(5 or 67 or True or 999 or [])
+# print([] or 0 or (6, 7, 4) or 999 or None)
+# print(5 and 67 and 0 and 999 and [])
+# print([56, 89] and 10 and (6, 7, 4) and 999 and 111)
+
+# if [90, 78, 5] or 67 or True or 999 or []:
+#     print("888")
+
+############################################################
+# age = float(input("Enter your age: "))
+
+# # if age < 60:
+# #     print('adult')
+# # else:
+# #     print('you are not old')
+
+
+# print('adult' if age < 60 else 'you are not old')
+# i = 1
+# while i < 100:
+#     if i % 2 == 0:
+#         print(i, end=" ")
+#     i += 1
+
+print(67, 89, 78, sep = "@")
