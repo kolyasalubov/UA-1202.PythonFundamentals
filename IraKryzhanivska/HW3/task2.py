db35e6699fb3075f9f5750297cc4594950d1bdd8
Iryna Kryzhanivska <kryzhanivskairusya@gmail.com>
2024-05-26 23:49:23 +0300

    have done task2

---
+++ IraKryzhanivska/HW3/task2.py
@@ -0,0 +1,10 @@
+a=3579
+b = str(a)
+d=int(b[0])*int(b[1])*int(b[2])*int(b[3])
+print(d)
+
+reversed_number = b[: : -1]
+print(int(reversed_number))
+
+sorted_digits = sorted([9,7,5,3])
+print(sorted_digits)
---
+++ IraKryzhanivska/HW3/task3.py
@@ -0,0 +1,9 @@
+x = 25
+y = 7
+
+x = x + y
+y = x - y
+x = x - y
+
+print("x=", x)
+print("y=", y)
