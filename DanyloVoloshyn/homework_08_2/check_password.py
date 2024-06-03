73af4eaebc7cd60722a1461ddd940b4cccd3f032
Danylo Voloshyn <danilovoloshyn@gmail.com>
2024-06-03 15:41:12 +0300

    Homework 08.2

--- DanyloVoloshyn/homework_08_2/check_password.py
+++ DanyloVoloshyn/homework_08_2/check_password.py
@@ -1,13 +1,15 @@
 def check_password(password: str):
+    error_messages = []
+
     if len(password) < 6 or len(password) > 16:
-        return False
+        error_messages.append("\nPassword must be between 6 and 16 characters long")
 
     has_lower = False
     has_upper = False
     has_digit = False
     has_special = False
 
-    special_charactres = "$#@"
+    special_characters = "$#@"
 
     for char in password:
         if char.islower():
@@ -16,23 +18,22 @@ def check_password(password: str):
             has_upper = True
         elif char.isdigit():
             has_digit = True
-        elif char in special_charactres:
+        elif char in special_characters:
             has_special = True
 
     if not has_lower:
-        print("There must be at least one lowercase letter in the password")
-        return False
+        error_messages.append("There must be at least one lowercase letter in the password")
     if not has_upper:
-        print("There must be at least one uppercase letter in the password")
-        return False
+        error_messages.append("There must be at least one uppercase letter in the password")
     if not has_digit:
-        print("There must be at least one digit in the password")
-        return False
+        error_messages.append("There must be at least one digit in the password")
     if not has_special:
-        print("There must be at least one special symbol in the password")
-        return False
+        error_messages.append("There must be at least one special symbol in the password($#@)")
 
-    return True
+    if error_messages:
+        return False, error_messages
+    else:
+        return True, []
 
 
 while True:
@@ -42,11 +43,13 @@ while True:
         print("\nGoodbye!")
         break
 
-    validation_result = check_password(input_message)
+    is_valid, messages = check_password(input_message)
 
-    if validation_result is True:
+    if is_valid:
         print("\nYour password is correct!")
         print("Thank you, have a good day!")
         break
     else:
-        print(f"\nYour password is not correct!")
+        print("\nYour password is not correct!")
+        for message in messages:
+            print(message)
