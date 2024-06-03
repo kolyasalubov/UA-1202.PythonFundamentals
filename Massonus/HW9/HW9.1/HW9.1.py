176c065ab737d01d6a6f01bb41d84f63d5c3e2a0
“Massonus“ <maksym.lithuanian@gmail.com>
2024-06-03 13:36:21 +0300

    completed hw 9.1

--- Massonus/HW9/HW9.1/HW9.1.py
+++ Massonus/HW9/HW9.1/HW9.1.py
@@ -3,7 +3,7 @@ import random
 
 FPS = 60
 
-WIDTH_DISPLAY = 700
+WIDTH_DISPLAY = 850
 HEIGHT_DISPLAY = 400
 
 BLACK = (0, 0, 0)
@@ -11,7 +11,7 @@ BLACK = (0, 0, 0)
 pygame.init()
 gameDisplay = pygame.display.set_mode((WIDTH_DISPLAY, HEIGHT_DISPLAY))
 
-pygame.display.set_caption("Homework test")
+pygame.display.set_caption("Guess the number")
 clock = pygame.time.Clock()
 
 pygame.display.update()
@@ -24,17 +24,27 @@ input_active = True
 running = True
 
 random_number = random.randint(1, 100)
+attempts = 10
 
 
 def check_user_input(user_number):
     user_number = int(user_number)
     if user_number > random_number:
-        return "The number is too high"
+        return f"{user_input} > the number. Press any key to continue"
     elif user_number < random_number:
-        return "The number is too low"
+        return f"{user_input} < the number. Press any key to continue"
     elif user_number == random_number:
         return "The number is correct. Congratulations!"
 
+
+def print_text(text):
+    gameDisplay.fill(BLACK)
+    text_surf = font.render(text, True, (255, 0, 0))
+    gameDisplay.blit(text_surf, text_surf.get_rect(center=gameDisplay.get_rect().center))
+    pygame.display.flip()
+
+
+print_text("Input text from 1 to 100: ")
 
 while running:
 
@@ -49,21 +59,25 @@ while running:
             input_active = True
             user_input = ""
 
+        if attempts == 0:
+            input_active = False
+            print_text("No attempts left. Close the game and try again")
+
         elif event.type == pygame.KEYDOWN and input_active:
 
             if event.key == pygame.K_RETURN:
-                input_active = False
+                input_active = True
+                print_text(check_user_input(user_input))
+                user_input = ""
+                attempts -= 1
+                print(attempts)
 
             elif event.key == pygame.K_BACKSPACE:
                 user_input = user_input[:-1]
-
+                print_text("Input text from 1 to 100: " + user_input)
             else:
                 user_input += event.unicode
-
-    gameDisplay.fill(BLACK)
-    text_surf = font.render("Input number from 1 to 100: " + user_input, True, (255, 0, 0))
-    gameDisplay.blit(text_surf, text_surf.get_rect(center=gameDisplay.get_rect().center))
-    pygame.display.flip()
+                print_text("Input text from 1 to 100: " + user_input)
 
 pygame.quit()
 exit()
