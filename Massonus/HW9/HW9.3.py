99a4070492f0bea9b58b5a2478d4f6a0b320d6d1
“Massonus“ <maksym.lithuanian@gmail.com>
2024-06-03 16:08:01 +0300

    completed HW 9.3

--- Massonus/HW9/HW9.3.py
+++ Massonus/HW9/HW9.3.py
@@ -6,13 +6,17 @@ WIDTH = 450
 
 root = tk.Tk()
 
-clouds = "Test"
-
 
 def get_weather(name):
-    global clouds
-    clouds = get_weather_data(name).get("clouds")
-    text1.config(text='Clouds = ' + clouds)
+    text1.config(text=f'Detailed status: {get_weather_data(name).get("clouds")}')
+    text2.config(text=f'Wind speed: {get_weather_data(name).get("wind_speed")}, '
+                      f'Wind direction: {get_weather_data(name).get("deg")}')
+    text3.config(text=f'Humidity: {get_weather_data(name).get("humidity")}')
+    text4.config(text=f'°C: max: {get_weather_data(name).get("temperature").get('temp_max')}, '
+                      f'min: {get_weather_data(name).get("temperature").get('temp_min')}, '
+                      f'avg: {get_weather_data(name).get("temperature").get('temp')}')
+    text5.config(text=f'Rain: {get_weather_data(name).get("rain")}')
+    text6.config(text=f'Clouds: {get_weather_data(name).get("clouds")}')
 
     text1.pack(padx=10, pady=10)
     text2.pack(padx=10, pady=10)
@@ -42,13 +46,11 @@ button.place(relx=0.7, rely=0, relwidth=0.3, relheight=1)
 frame1 = tk.LabelFrame(text='Weather data', bg='gold', width=400, height=200)
 frame1.pack()
 
-text1 = tk.Label(frame1, text='' + clouds, width=40, height=1, font=('Courier', 12))
+text1 = tk.Label(frame1, text='', width=40, height=1, font=('Courier', 12))
 text2 = tk.Label(frame1, text='', width=40, height=1, font=('Courier', 12))
 text3 = tk.Label(frame1, text='', width=40, height=1, font=('Courier', 12))
 text4 = tk.Label(frame1, text='', width=40, height=1, font=('Courier', 12))
 text5 = tk.Label(frame1, text='', width=40, height=1, font=('Courier', 12))
 text6 = tk.Label(frame1, text='', width=40, height=1, font=('Courier', 12))
-
-
 
 root.mainloop()
--- Massonus/HW9/weather_utils.py
+++ Massonus/HW9/weather_utils.py
@@ -1,28 +1,19 @@
 from pyowm import OWM
 
 API_KEY = 'ef2206ff5da67de63306d0b143e20872'
-# ---------- FREE API KEY examples ---------------------
 
 owm = OWM(API_KEY)
 mgr = owm.weather_manager()
 
 
 def get_weather_data(location):
-    # Search for current weather in London (Great Britain) and get details
     observation = mgr.weather_at_place(location)
     w = observation.weather
-    return {"detailed_status": str(w.detailed_status),
-            "wind": str(w.wind),
+    return {"detailed_status": w.detailed_status,
+            "wind_speed": w.wind().get('speed'),
+            "wind_deg": w.wind().get('deg'),
             "humidity": w.humidity,
             "temperature": w.temperature("celsius"),
             'rain': w.rain,
             "heat_index": w.heat_index,
-            "clouds": str(w.clouds)}
-
-    # 'clouds'
-    # {'speed': 4.6, 'deg': 330}
-    # 87
-    # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-    # {}
-    # None
-    # 75
+            "clouds": w.clouds}
