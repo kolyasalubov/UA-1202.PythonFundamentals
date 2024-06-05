20ebd01ff6f20d848acee0c4c328229e0cb06eb0
“Massonus“ <maksym.lithuanian@gmail.com>
2024-06-05 13:22:12 +0300

    refactor

--- Massonus/HW10/HW10.2/task_6.py
+++ Massonus/HW10/HW10.2/task_6.py
@@ -1,3 +1,4 @@
 def class_name_changer(cls, new_name):
-    assert new_name[0].isupper() and new_name.isalnum()
+    if not new_name[0].isupper() or not new_name.isalnum():
+        raise Exception("Name must start with uppercase and contain only letters and numbers")
     cls.__name__ = new_name
